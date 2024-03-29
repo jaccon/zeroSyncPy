b1802bae14330a8847fd0d1c9d0e014f327b3dd3
Andre Jaccon <andrejaccon@natura.net>
2024-03-29 17:50:17 -0300

    test4

--- test1.py
+++ test1.py
@@ -6,11 +6,20 @@ import websockets
 logging.basicConfig(level=logging.INFO,
                     format='%(asctime)s - %(levelname)s - %(message)s')
 
-async def sync_files(websocket, path):
+async def websocket_handler(websocket, path):
+    if path == "/server":
+        await handle_server(websocket)
+    elif path == "/client":
+        await handle_client(websocket)
+
+async def handle_server(websocket):
     monitored_directory = "syncFolder"
     initial_files = set(os.listdir(monitored_directory))
 
     try:
+        remote_address = websocket.remote_address[0]
+        logging.info(f"Conectado com: {remote_address}")
+
         async for message in websocket:
             logging.info(f"Mensagem recebida: {message}")
             if message == "sync":
@@ -24,7 +33,26 @@ async def sync_files(websocket, path):
     except websockets.exceptions.ConnectionClosedError:
         logging.error("Conexão fechada inesperadamente.")
 
-start_server = websockets.serve(sync_files, "localhost", 8765)
+async def handle_client(websocket):
+    await websocket.send("Teste de conexão")
+    response = await websocket.recv()
+    logging.info(f"Resposta: {response}")
 
-asyncio.get_event_loop().run_until_complete(start_server)
-asyncio.get_event_loop().run_forever()
+async def start_server():
+    server = await websockets.serve(websocket_handler, "localhost", 8765)
+    logging.info("Servidor WebSocket iniciado.")
+    await server.wait_closed()
+
+async def start_client():
+    async with websockets.connect('ws://localhost:8765/server') as websocket:
+        await handle_client(websocket)
+
+if __name__ == "__main__":
+    mode = input("Selecione 'A' para servidor ou 'B' para cliente: ").strip().upper()
+
+    if mode == 'A':
+        asyncio.get_event_loop().run_until_complete(start_server())
+    elif mode == 'B':
+        asyncio.get_event_loop().run_until_complete(start_client())
+    else:
+        print("Modo inválido. Use 'A' para servidor ou 'B' para cliente.")
